6bce6db786d4c81aa216798d3916b334567bdd9a
auttapon.c <auttaponcoe24@gmail.com>
2024-06-11 22:16:43 +0700

    editpreview and setup dialog or modal

--- src/app/(routes)/stock/edit/page.tsx
+++ src/app/(routes)/stock/edit/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import StockEditForm from "./StockEditForm";
+import { doGetStockById } from "@/src/services/serverService";
+import { ProductData } from "@/src/models/product.model";
 
-type Props = {};
+type Props = {
+	searchParams: {
+		id?: string;
+	};
+};
 
-export default function StockEdit({}: Props) {
-	return <div>StockEdit</div>;
+export default async function StockEdit({ searchParams }: Props) {
+	let product = {} as ProductData;
+
+	if (searchParams.id) {
+		product = await doGetStockById(searchParams.id);
+		console.log("srr fetch edit", JSON.stringify(product));
+	}
+	return (
+		<div>
+			<StockEditForm product={product} />
+		</div>
+	);
 }
--- src/app/(routes)/stock/page.tsx
+++ src/app/(routes)/stock/page.tsx
@@ -17,7 +17,20 @@ import dayjs from "dayjs";
 
 import Zoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
-import { Fab, IconButton, Link, Stack, Typography } from "@mui/material";
+import {
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	Fab,
+	Grid,
+	IconButton,
+	Link,
+	Stack,
+	Typography,
+} from "@mui/material";
 import { NumericFormat } from "react-number-format";
 import {
 	Add,
@@ -29,15 +42,24 @@ import {
 	Star,
 } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
+import { userSelector } from "@/src/store/slices/userSlice";
+import StockCard from "@/src/app/_components/common/StockCard";
+import { ProductData } from "@/src/models/product.model";
 
 export default function Stock() {
-	const reducer = useSelector(productSelector);
+	const productReducer = useSelector(productSelector);
+	const userReducer = useSelector(userSelector);
 	const dispatch = useAppDispatch();
 	const router = useRouter();
+	const [selectedProduct, setSelectedProduct] =
+		React.useState<ProductData | null>(null);
+	const [openDialog, setOpenDialog] = React.useState<boolean>(false);
 
 	React.useEffect(() => {
-		dispatch(getProducts());
-	}, [dispatch]);
+		if (!userReducer.isAuthenticating) {
+			dispatch(getProducts());
+		}
+	}, [dispatch, userReducer.isAuthenticating]);
 
 	const columns: GridColDef<any>[] = [
 		{
@@ -142,7 +164,13 @@ export default function Stock() {
 						>
 							<Edit fontSize="inherit" />
 						</IconButton>
-						<IconButton aria-label="delete" size="large" onClick={() => {}}>
+						<IconButton
+							aria-label="delete"
+							size="large"
+							onClick={() => {
+								setOpenDialog(true);
+							}}
+						>
 							<Delete fontSize="inherit" />
 						</IconButton>
 					</Stack>
@@ -170,10 +198,93 @@ export default function Stock() {
 		</GridToolbarContainer>
 	);
 
+	const handleDeleteConfirm = () => {};
+
+	// Dialog or Modal
+	const showDialog = () => {
+		// if (selectedProduct === null) {
+		// 	return;
+		// }
+		return (
+			<Dialog
+				open={openDialog}
+				keepMounted
+				aria-labelledby="alert-dialog-slide-title"
+				aria-describedby="alert-dialog-slide-description"
+			>
+				<DialogTitle id="alert-dialog-slide-title">
+					<Image
+						width={100}
+						height={100}
+						alt="product image"
+						// src={productImageURL(selectedProduct.image)}
+						style={{ width: 100, borderRadius: "5%", objectFit: "cover" }}
+					/>
+					<br />
+					{/* Confirm to delete the product? : {selectedProduct.name} */}
+				</DialogTitle>
+				<DialogContent>
+					<DialogContentText id="alert-dialog-slide-description">
+						You cannot restore deleted product.
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={() => setOpenDialog(false)} color="info">
+						Cancel
+					</Button>
+					<Button onClick={handleDeleteConfirm} color="primary">
+						Delete
+					</Button>
+				</DialogActions>
+			</Dialog>
+		);
+	};
+
 	return (
 		<Box sx={{ height: 400, width: "100%" }}>
+			{/* Cards */}
+			<Grid container style={{ marginBottom: 16 }} spacing={7}>
+				<Grid item lg={3} md={6} sm={12}>
+					<StockCard
+						icon={AddShoppingCart}
+						title="TOTAL"
+						subtitle="112 THB"
+						color="#00a65a"
+					/>
+				</Grid>
+
+				<Grid item lg={3} md={6} sm={12}>
+					<StockCard
+						icon={NewReleases}
+						title="EMPTY"
+						subtitle="9 PCS."
+						color="#f39c12"
+					/>
+				</Grid>
+
+				<Grid item lg={3} md={6} sm={12}>
+					<StockCard
+						icon={AssignmentReturn}
+						title="RETURN"
+						subtitle="1 PCS."
+						color="#dd4b39"
+					/>
+				</Grid>
+
+				<Grid item lg={3} md={6} sm={12}>
+					<StockCard
+						icon={Star}
+						title="LOSS"
+						subtitle="5 PCS."
+						color="#00c0ef"
+					/>
+				</Grid>
+			</Grid>
+
+			{/* Table */}
 			<DataGrid
-				rows={reducer}
+				sx={{ backgroundColor: "white", height: "70vh" }}
+				rows={productReducer}
 				columns={columns}
 				initialState={{
 					pagination: {
@@ -187,6 +298,9 @@ export default function Stock() {
 					toolbar: CustomToolbar,
 				}}
 			/>
+
+			{/* Dialog or Modal */}
+			{showDialog()}
 		</Box>
 	);
 }
